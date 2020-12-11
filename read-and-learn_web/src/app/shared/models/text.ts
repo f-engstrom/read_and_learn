410eb3cde783fed5f5bd4efb598eb7e413d4571d
Frans Engström <frans.engstrom@learnet.se>
2020-12-11 17:34:09 +0100

    add url slug to text

--- read-and-learn_web/src/app/shared/models/text.ts
+++ read-and-learn_web/src/app/shared/models/text.ts
@@ -1,7 +1,13 @@
 export class Text{
 
   constructor(public id:string, public name:string,public body:string, public nrOfWords:number ) {
-  }
 
+    this.urlSlug = this.makeUrlSlug(name);
+  }
+  public urlSlug:string;
+
+  makeUrlSlug(name:string){
+    return name.replace(/ /g, "-").toLowerCase();
+  }
 
 }
